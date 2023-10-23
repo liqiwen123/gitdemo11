63424043ad446caf58df16159b03a1eeefccbaab
liqiwen0731 <3052938629@qq.com>
2023-10-23 20:47:29 +0800

    新增java模块

---
+++ demo/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @ProjectName ${PROJECT_NAME}
+ * @Description TODO
+ * @Version 1.0
+ * @Author liqiwen
+ * @Time ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
